d60e3484b7b79db29ca09099f1115d3ea825ba5f
waylonwilliams <crazyocean9@gmail.com>
2024-10-11 13:00:43 -0700

    add image to background and bottom sheet component rendered

--- app/(tabs)/cafe.tsx
+++ app/(tabs)/cafe.tsx
@@ -1,19 +1,62 @@
 import { router } from "expo-router";
-import { Pressable, SafeAreaView, Text } from "react-native";
-
+import { Image, Pressable, SafeAreaView, Text, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import { useCallback, useMemo, useRef } from "react";
+/**
+ * When you click on a cafe card / pin on map this page will be shown
+ * Ideally upgrade this to take in a cafe as a param and render it that way
+ */
 export default function CafeLayout() {
+  // idk stuff for the bottom sheet
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log("handleSheetChanges", index);
+  }, []);
+  const snapPoints = useMemo(() => ["75%", "100%"], []);
+
   return (
     <SafeAreaView
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        position: "relative",
+        backgroundColor: "#white",
       }}
     >
-      <Pressable onPress={() => router.back()}>
-        <Text>Return</Text>
-      </Pressable>
-      <Text>Meow.</Text>
+      <View
+        style={{ backgroundColor: "#f0f0f0", height: "100%", width: "100%" }}
+      >
+        <Image
+          style={{ top: -60, width: "100%", position: "absolute" }}
+          source={require("../../assets/images/Oshima.png")}
+        />
+
+        <Pressable onPress={() => router.back()}>
+          <Ionicons
+            name="chevron-back"
+            size={24}
+            color="white"
+            style={{ padding: 4, position: "absolute", zIndex: 2 }}
+          />
+        </Pressable>
+
+        <BottomSheet
+          ref={bottomSheetRef}
+          onChange={handleSheetChanges}
+          index={0}
+          snapPoints={snapPoints}
+        >
+          <BottomSheetView
+            style={{
+              padding: 24,
+            }}
+          >
+            <Text>Here i am</Text>
+          </BottomSheetView>
+        </BottomSheet>
+      </View>
     </SafeAreaView>
   );
 }
