37ce98a79d3445c0227e8fc82fa0f8f33257e35f
nhi1e <tnhile2004@gmail.com>
2024-10-14 10:50:54 -0700

    works when built but not when run

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,5 +1,17 @@
 import { Text, SafeAreaView, StyleSheet } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+const styles = StyleSheet.create({
+	container: {
+		...StyleSheet.absoluteFillObject,
+		height: 400,
+		width: 400,
+		justifyContent: "flex-end",
+		alignItems: "center",
+	},
+	map: {
+		...StyleSheet.absoluteFillObject,
+	},
+});
 export default function Index() {
 	return (
 		<SafeAreaView
