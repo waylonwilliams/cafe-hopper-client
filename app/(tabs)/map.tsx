a66e41332d7fdfb534db9761c8eec77c78d8a2e9
nhi1e <tnhile2004@gmail.com>
2024-10-13 21:31:46 -0700

    ??

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,15 +1,24 @@
-import { Text, SafeAreaView } from "react-native";
-
+import { Text, SafeAreaView, StyleSheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 export default function Index() {
-  return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Map page</Text>
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView
+			style={{
+				flex: 1,
+				// justifyContent: "center",
+				// alignItems: "center",
+			}}
+		>
+			<MapView
+				provider={PROVIDER_GOOGLE}
+				style={StyleSheet.absoluteFillObject}
+				initialRegion={{
+					latitude: 36.9940514,
+					longitude: -122.0650572,
+					latitudeDelta: 0.0922,
+					longitudeDelta: 0.0421,
+				}}
+			/>
+		</SafeAreaView>
+	);
 }
