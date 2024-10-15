166cd043a0a4c27e191cc6aea8c9362bf0395486
nhi1e <tnhile2004@gmail.com>
2024-10-15 15:27:03 -0700

    apple map showing with redirect button to user location

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,15 +1,91 @@
-import { Text, SafeAreaView } from "react-native";
+import {
+	Text,
+	View,
+	SafeAreaView,
+	StyleSheet,
+	Dimensions,
+	TouchableOpacity,
+} from "react-native";
+import MapView, { PROVIDER_GOOGLE, Marker, Region } from "react-native-maps";
+import React, { useEffect, useState, useRef } from "react";
+import * as Location from "expo-location";
+import { useNavigation } from "@react-navigation/native";
+import { MaterialIcons } from "@expo/vector-icons";
 
-export default function Index() {
-  return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Map page</Text>
-    </SafeAreaView>
-  );
+export default function Map() {
+	const [mapRegion, setMapRegion] = useState({
+		latitude: 5.603717,
+		longitude: -0.186964,
+		latitudeDelta: 0.005,
+		longitudeDelta: 0.005,
+	});
+
+	const mapRef = useRef<MapView>(null); // Reference to the MapView
+
+	const userLocation = async () => {
+		let { status } = await Location.requestForegroundPermissionsAsync();
+		if (status !== "granted") {
+			console.log("Permission to access location was denied");
+			return;
+		}
+		let location = await Location.getCurrentPositionAsync({
+			accuracy: Location.Accuracy.High,
+		});
+		const newRegion = {
+			latitude: location.coords.latitude,
+			longitude: location.coords.longitude,
+			latitudeDelta: 0.005,
+			longitudeDelta: 0.005,
+		};
+		setMapRegion(newRegion);
+
+		// Animate the map to the new region
+		if (mapRef.current) {
+			mapRef.current.animateToRegion(newRegion, 1000);
+		}
+	};
+
+	useEffect(() => {
+		userLocation();
+	}, []);
+
+	return (
+		<View style={{ flex: 1 }}>
+			<TouchableOpacity style={styles.location} onPress={userLocation}>
+				<Text>
+					<MaterialIcons name="my-location" size={24} color="black" />
+				</Text>
+			</TouchableOpacity>
+			<MapView
+				ref={mapRef} // Attach reference to MapView
+				style={styles.map}
+				initialRegion={mapRegion}
+				region={mapRegion}
+				// zoomEnabled={true}
+				followsUserLocation={true}
+				showsMyLocationButton={true}
+				mapType="standard"
+			>
+				<Marker coordinate={mapRegion} title="i'm here" />
+			</MapView>
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	map: {
+		width: Dimensions.get("window").width,
+		height: Dimensions.get("window").height,
+	},
+	location: {
+		position: "absolute",
+		zIndex: 50,
+		bottom: 10,
+		right: 10,
+		backgroundColor: "white",
+		padding: 10,
+		borderRadius: 50,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+});
