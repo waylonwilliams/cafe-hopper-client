ab8f636047e3687a8d6ea38782879432bdb8aac3
nhi1e <tnhile2004@gmail.com>
2024-10-23 17:27:28 -0700

    using new icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,38 +1,33 @@
-import { Tabs } from "expo-router";
-import React from "react";
-import { TabBar } from "../../components/TabBar";
-
-import { TabBarIcon } from "@/components/TabBarIcon";
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { TabBar } from '../../components/TabBar';
 
 export default function TabLayout() {
-	return (
-		<Tabs
-			screenOptions={{
-				headerShown: false,
-			}}
-			tabBar={(props) => <TabBar {...props} />}
-		>
-			<Tabs.Screen
-				name="index"
-				options={{
-					title: "Home",
-					// This title is displayed on the tab
-				}}
-			/>
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+      tabBar={(props) => <TabBar {...props} />}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+        }}
+      />
 
-			<Tabs.Screen
-				name="explore"
-				options={{
-					title: "Explore",
-				}}
-			/>
-			<Tabs.Screen
-				name="profile"
-				options={{
-					title: "Profile",
-				}}
-			/>
-			{/* Exclude cafe.tsx from Tabs */}
-		</Tabs>
-	);
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+        }}
+      />
+    </Tabs>
+  );
 }
--- components/TabBarIcon.tsx
+++ components/TabBarIcon.tsx
@@ -1,9 +1,15 @@
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 
 import Ionicons from '@expo/vector-icons/Ionicons';
+import Feather from '@expo/vector-icons/Feather';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
 import { type IconProps } from '@expo/vector-icons/build/createIconSet';
 import { type ComponentProps } from 'react';
 
-export function TabBarIcon({ style, ...rest }: IconProps<ComponentProps<typeof Ionicons>['name']>) {
-  return <Ionicons size={28} style={[{ marginBottom: -3 }, style]} {...rest} />;
+export function TabBarIcon({
+  style,
+  ...rest
+}: IconProps<ComponentProps<typeof MaterialIcons>['name']>) {
+  return <MaterialIcons size={28} style={[{ marginBottom: -3 }, style]} {...rest} />;
 }
