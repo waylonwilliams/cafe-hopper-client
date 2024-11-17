df0c26bfc92a90bb9b38d3eb5b0ef2369c1cc76f
Harshith Pothula <70039379+hpothula983@users.noreply.github.com>
2024-11-16 17:00:30 -0800

    Commented console.log()

--- app/login.tsx
+++ app/login.tsx
@@ -21,8 +21,8 @@ const LoginScreen = () => {
   const [loading, setLoading] = useState(false);
 
   async function signInWithEmail() {
-    console.log('Email:', email);
-    console.log('Password:', password);
+    // console.log('Email:', email);
+    // console.log('Password:', password);
     setLoading(true);
     const { error } = await supabase.auth.signInWithPassword({
       email: email,
@@ -84,7 +84,7 @@ const LoginScreen = () => {
           <Image source={require('@/assets/images/arrow.png')} />
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => router.replace('/signUp')}>
+        <TouchableOpacity onPress={() => router.replace('@/app/signUp')}>
           <Text>Already have an account? Sign Up</Text>
         </TouchableOpacity>
       </View>
