bd0062cc8d4590c4d0879e4b199cd5409963f5ea
waylonwilliams <crazyocean9@gmail.com>
2024-11-21 22:02:47 -0800

    add back button to antoherUserProfile

--- app/anotherUserProfile.tsx
+++ app/anotherUserProfile.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useLocalSearchParams } from 'expo-router';
-import { SafeAreaView, View } from 'react-native';
+import { router, useLocalSearchParams } from 'expo-router';
+import { Pressable, SafeAreaView, View } from 'react-native';
 import { useState } from 'react';
 import Profile from '../components/Profile';
 import ImageFullView from '@/components/CafePage/ImageFullView';
 import assertString from '@/components/assertString';
+import { Ionicons } from '@expo/vector-icons';
 
 // This page won't be navigated to if they are an admin
 // This is just used when they click on a review and it is not their profile
@@ -19,6 +20,15 @@ export default function anotherUserProfile() {
   return (
     <>
       <SafeAreaView style={{ position: 'relative' }}>
+        <View style={{ width: '100%' }}>
+          <Pressable
+            onPress={() => router.back()}
+            style={{
+              padding: 10,
+            }}>
+            <Ionicons name="chevron-back" size={24} color="black" />
+          </Pressable>
+        </View>
         <Profile uid={uid} setViewingImages={setViewingImages} />
       </SafeAreaView>
       {viewingImages !== null && (
